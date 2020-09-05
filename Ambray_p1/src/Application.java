4a1a68d1851fdf1cda3f69e5486929b44abcaac2
Luke Ambray <lukeambray@knights.ucf.edu>
2020-09-04 21:55:00 -0400

    Finished application

--- Ambray_p1/src/Application.java
+++ Ambray_p1/src/Application.java
@@ -8,24 +8,40 @@ public class Application {
         Scanner input = new Scanner(System.in);
 
         //prints prompt for user
-        
+        System.out.println("1 - Encrypt");
+        System.out.println("2 - Decrypt");
+        System.out.println("STOP - End program");
 
         //while-loop
         String thing = "";
         while(!thing.equalsIgnoreCase("stop")) {
 
+            System.out.println(); // outputs a blank line
+            System.out.printf("Please select an operation: ");
+            thing = input.nextLine();
+            System.out.println();
 
+            if(thing.equals("1")){
+
+                //creates an Encypter object
+                System.out.println("You selected: Encryption");
+                Encrypter myEncrypter = new Encrypter();
+                System.out.printf("Please enter an integer string: ");
+                String theInput = input.nextLine();
+                myEncrypter.encrypt(theInput);
+                System.out.println(); // outputs a blank line
+
+            } else if(thing.equals("2")){
+
+                //creates an Decypter object
+                System.out.println("You selected: Decryption");
+                Decrypter myDecrypter = new Decrypter();
+                System.out.printf("Please enter an integer string: ");
+                String theInput = input.nextLine();
+                myDecrypter.decrypt(theInput);
+                System.out.println(); // outputs a blank line
+            } else break;
         }
-        //creates an Encypter object
-        //Encrypter myEncrypter = new Encrypter();
-
-        //creates an Decypter object
-        Decrypter myDecrypter = new Decrypter();
-
-        System.out.printf("Please enter an integer string: ");
-        String theInput = input.nextLine();
-        myDecrypter.decrypt(theInput);
-
-        System.out.println(); // outputs a blank line
+        System.out.println("You ended the program.");
     }
 }
