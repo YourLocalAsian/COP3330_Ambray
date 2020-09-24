dca6b23cdfad7a73dcc6bd8e17413820fb6b4054
Luke Ambray <johndoe@example.com>
2020-09-24 14:03:53 -0400

    Started prototyping App

--- Ambray_p2/src/App.java
+++ Ambray_p2/src/App.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+
+    Scanner userInput = new Scanner;
+
     public static void main(String[] args) {
         ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
 
@@ -13,5 +19,31 @@ public class App {
         }
 
         displayBmiStatistics(bmiData);
+    }
+
+    //moreInput - prompts Y or N and returns bool
+    public static boolean moreInput(){
+        return true;
+    }
+
+
+    public static int getUserHeight(){
+    //prompts for height in inches & only accepts positive values
+        //create local variable and set it to inputDouble
+        //if > 0, return input
+        //else print prompt for valid value
+            //call function again
+    }
+
+    public static int getUserWeight(){
+    //prompts for weight in lbs and only accepts positive values
+    }
+
+    public static void displayBmiInfo(BodyMassIndex bmi_x){
+    //prints out a user's BMi score and category using the data contained in a BodyMassIndex object
+    }
+
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmi_y){
+    //prints out the average BMI score of the data
     }
 }
